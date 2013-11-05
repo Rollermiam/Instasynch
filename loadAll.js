471544907508773a42592c98012b136ebb0c48b8
Rollermiam <Rollermaniac@live.fr>
2013-11-05 11:11:42 +0100

    Update loadAll.js

--- loadAll.js
+++ loadAll.js
@@ -13,5 +13,5 @@ window.onload=function onload(){
     if(oldOnload){
 	   oldOnload();
     }
-    $.getScript('https://github.com/Rollermiam/Instasynch/blob/patch-1/deploy.js');
+    $.getScript('https://raw.github.com/Bibbytube/Instasynch/master/deploy.js'');
 };
